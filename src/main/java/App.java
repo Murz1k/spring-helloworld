fb536e946b64d108bbcc0828200503f250df4a21
Murz1k <69905294+Murz1k@users.noreply.github.com>
2022-01-08 15:12:01 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,27 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+    private static Cat testCat1;
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+
+        Cat testCat = (Cat) applicationContext.getBean("cat");
+        Cat testCat1 = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(bean == bean1);
+        System.out.println(testCat == testCat1);
+
+
     }
+
 }
